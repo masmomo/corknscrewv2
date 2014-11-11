ad19c6e3cb59638e70108196145df879057d43f9
Nick Yudha (MacBook Pro) <nick.yudha@gmail.com>
2014-11-11 12:40:38 +0700

    Navbar Update
    Removing newsletter

--- static/navbar.php
+++ static/navbar.php
@@ -7,13 +7,13 @@
         <li class="<?php if($act == '_about/index'){ echo 'active';}else{ echo 'nav-1';}?>"><a href="<?php echo $prefix_url;?>about">About Us</a></li>
         <li class="<?php if($act == '_food-drinks/index'){ echo 'active';}else{ echo 'nav-1';}?>"><a href="<?php echo $prefix_url;?>food-drinks">Food &amp; Drinks</a></li>
         <li class="<?php if($act == '_wines/index'){ echo 'active';}else{ echo 'nav-1';}?>"><a href="<?php echo $prefix_url;?>wines">Wines</a></li>
-        <li class="<?php if($act == '_newsletter/index'){ echo 'active';}else{ echo 'nav-1';}?>"><a href="<?php echo $prefix_url;?>newsletter">Newsletter</a></li>
-        
+        <!--<li class="<?php if($act == '_newsletter/index'){ echo 'active';}else{ echo 'nav-1';}?>"><a href="<?php echo $prefix_url;?>newsletter">Newsletter</a></li>-->
+        <li class="<?php if($act == '_events/index'){ echo 'active';}else{ echo 'nav-2';}?>"><a href="<?php echo $prefix_url;?>events">Events</a></li>
+
         <a href="<?php echo $prefix_url;?>" class="navbar-brand">
           <img src="<?php echo $prefix_url;?>assets/img/logo.png" alt="logo" width="100%">
         </a>
-        
-        <li class="<?php if($act == '_events/index'){ echo 'active';}else{ echo 'nav-2';}?>"><a href="<?php echo $prefix_url;?>events">Events</a></li>
+      
         <li class="<?php if($act == '_gallery/index'){ echo 'active';}else{ echo 'nav-2';}?>"><a href="<?php echo $prefix_url;?>gallery">Gallery</a></li>
         <li class="<?php if($act == '_contacts/index'){ echo 'active';}else{ echo 'nav-2';}?>"><a href="<?php echo $prefix_url;?>contacts">Contacts</a></li>
         <li class="<?php if($act == '_catering/index'){ echo 'active';}else{ echo 'nav-2';}?>"><a href="<?php echo $prefix_url;?>catering">Catering</a></li>
